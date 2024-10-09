db2cdbac8acda3b42a5dc09e2feb4ed410285cdf
jonbk <jonathan.buyck@tiime.fr>
2024-10-09 18:49:44 +0200

    dotenv

--- public/index.php
+++ public/index.php
@@ -4,6 +4,10 @@ use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
+$_SERVER['APP_RUNTIME_OPTIONS'] = [
+    'disable_dotenv' => $_SERVER['APP_ENV'] === 'prod',
+];
+
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
